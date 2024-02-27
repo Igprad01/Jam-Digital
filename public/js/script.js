d65e05670679badfe653b0457e334dee606fd810
igprad01 <igustiputra99@gmail.com>
2024-02-27 11:08:00 +0700

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+const jam = document.getElementById('jam');
+const menit = document.getElementById('menit');
+const detik = document.getElementById('detik');
+
+function JamDigital() {
+    let date = new date();
+    let hours = get.hours();
+    // dilanjutkan nanti masih ga focus 
+}
