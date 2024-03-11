f9d0fc4befcbc1f901538119528a85385852121d
igprad01 <igustiputra99@gmail.com>
2024-03-11 16:01:33 +0700

    add tgl

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,27 @@
 const jam = document.getElementById('jam');
 const menit = document.getElementById('menit');
 const detik = document.getElementById('detik');
+const tanggal = document.getElementById('tanggal');
+const bulan = document.getElementById('bulan');
+const tahun = document.getElementById('tahun');
+
 
 function JamDigital() {
+    const namaBulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];  // lanjutkan nanti
     const tgl = new Date();
     const hours = tgl.getHours();
     const dtk = tgl.getSeconds();
-    const mnt = tgl.getMinutes(); 
+    const mnt = tgl.getMinutes();
+    const date = tgl.getDate();
+    const getMonth = tgl.getMonth();
+    const month = namaBulan[getMonth];
+    const year = tgl.getFullYear();
     jam.textContent = `${hours}`;
     detik.textContent = `${dtk}`;
     menit.textContent = `${mnt}`;
+    tanggal.textContent = `${date}`;
+    bulan.textContent = `${month}`;
+    tahun.textContent = `${year}`;
 }
 
 setInterval(JamDigital);
